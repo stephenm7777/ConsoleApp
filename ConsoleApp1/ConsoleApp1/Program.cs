43e13908eaec6919c978de2f8c95db85b15a845a
Stephen Martinez <122754065+stephenm7777@users.noreply.github.com>
2023-12-24 05:34:28 -0800

    .NET

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+string name = typeof(Program).Namespace ?? "None"!;
+Console.WriteLine($"Namespace: {name}");
+throw new Exception();
